746ac01476831d54557e37202d9971d2224de36a
dwei-cn <darken.vi@hotmail.com>
2023-12-17 21:06:03 -0800

    update

--- react-app-xiaoma/reactweb/src/App.js
+++ react-app-xiaoma/reactweb/src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import Card1 from './components/Card1';
 import Card2 from './components/Card2';
@@ -35,28 +35,28 @@ function App() {
   // 父组件中定义数据
   const dataList = [
     {
-    image: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/96748826_102994418092727_8540014934442901504_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7f8c78&_nc_ohc=osPJR1lYn2UAX908RCD&_nc_ht=scontent-sjc3-1.xx&oh=00_AfC9I0Tr6L_bIgnMP9Uhk_E3mKIwvwQ-OWV-uEEBck3Msg&oe=65A4AC60",
-    title: "免费玩GTA5",
-    content: "上Epic Games免费玩GTA5哟",
-    link: "https://www.epicgames.com/store/en-US/product/epic-games-store/gta-5-free-edition"
-  },
+      image: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/96748826_102994418092727_8540014934442901504_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7f8c78&_nc_ohc=osPJR1lYn2UAX908RCD&_nc_ht=scontent-sjc3-1.xx&oh=00_AfC9I0Tr6L_bIgnMP9Uhk_E3mKIwvwQ-OWV-uEEBck3Msg&oe=65A4AC60",
+      title: "免费玩GTA5",
+      content: "上Epic Games免费玩GTA5哟",
+      link: "https://www.epicgames.com/store/en-US/product/epic-games-store/gta-5-free-edition"
+    },
 
-  {
-    image: "https://www.overclockers.co.uk/blog/wp-content/uploads/2023/05/Pokemon-TCG-Live-Blog-Feature.png",
-    title: "Pokemon TCG Live Blog Feature",
-    content: "This is a live blog feature for the Pokemon TCG.",
-    link: "https://tcg.pokemon.com/en-us/tcgl/"
-  },
-]
+    {
+      image: "https://www.overclockers.co.uk/blog/wp-content/uploads/2023/05/Pokemon-TCG-Live-Blog-Feature.png",
+      title: "Pokemon TCG Live Blog Feature",
+      content: "This is a live blog feature for the Pokemon TCG.",
+      link: "https://tcg.pokemon.com/en-us/tcgl/"
+    },
+  ]
 
   const listItems = dataList.map((itemData, index) =>   //  map 方法遍历一个名为 dataList 的数组，并为数组中的每个元素创建一个名为 ItemDetail2 的组件实例。
-  <Card2 
-    key = {index}
-    ItemTitle = {itemData.title}      // 应该和js func里面的props相对应
-    ItemImage = {itemData.image}
-    ItemDescription = {itemData.content}
-    ItemLink = {itemData.link}
-  />
+    <Card2
+      key={index}
+      ItemTitle={itemData.title}      // 应该和js func里面的props相对应
+      ItemImage={itemData.image}
+      ItemDescription={itemData.content}
+      ItemLink={itemData.link}
+    />
   )
 
   const [message, setMessage] = useState("Hello react form!");
@@ -67,7 +67,7 @@ function App() {
     txtRegionID: "Asia Pacific"
   }
 
-  const handlerAppDataEvent = async(appdata) => {
+  const handlerAppDataEvent = async (appdata) => {
     //console.log(appdata)
     //alert(JSON.stringify(appdata))
     const postdata = JSON.stringify({
@@ -84,7 +84,7 @@ function App() {
         'user-agent': 'Mozilla/99.0 MDN Example'
       },
     });
-    
+
     const data = await response.json()
     setMessage(JSON.stringify(data.body, {}, 2))  // 整个表达式的目的是将 data.body 对象转换为带有缩进格式的 JSON 字符串，并且这个字符串会在每一层级使用两个空格进行缩进。这通常用于美化输出，以便更容易阅读和调试。
   }
@@ -93,10 +93,10 @@ function App() {
 
   return (
     <div className="App">
-      <Header />    
-      
+      <Header />
+
       <h1>I love React!</h1>
-      
+
       <hr />
       <h2>单个卡片</h2>
       <Card1 />
@@ -106,17 +106,17 @@ function App() {
       {listItems}
 
       <hr />
-       {/* <h2>Form 表单</h2> */}
-        <AwsForm appdata={formData} onSave={handlerAppDataEvent}/>
-        <pre>
-          {message}
-        </pre>
+      {/* <h2>Form 表单</h2> */}
+      <AwsForm appdata={formData} onSave={handlerAppDataEvent} />
+      <pre>
+        {message}
+      </pre>
 
-        <hr />
-        <h2>To-do List App</h2>
-        <Todolist />
+      <hr />
+      <h2>To-do List App</h2>
+      <Todolist />
     </div>
-    
+
   );
 }
 
--- react-app-xiaoma/reactweb/src/components/Todolist.js
+++ react-app-xiaoma/reactweb/src/components/Todolist.js
@@ -1,10 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState, useRef, Fragment } from 'react';
 
 
 const Todolist = () => {
 
-    const [newItem, setNewItem] = useState("")    
+    const [newItem, setNewItem] = useState("")
     const [items, setItems] = useState([])     // 注意 setItems 是一个函数，要传入state变化后新的值， items则是每次user输入的值
+
+    const [newEditItem, setnewEditItem] = useState("")
+    const [editItemID, seteditItemID] = useState(0)
+
+    const editInputRef = useRef(null);
 
     const addItem = () => {
 
@@ -12,10 +17,10 @@ const Todolist = () => {
 
 
         //console.log(newItem)
-        if (!newItem) {
-            alert("Please enter a task")
-            return
-        }
+        // if (!newItem) {
+        //     alert("Please enter a task")
+        //     return
+        // }
 
         // 添加的是带有id的object，而不只是value
         const newItemObject = {
@@ -28,43 +33,85 @@ const Todolist = () => {
         setNewItem("")  // 每次click之后自动还原setNewItem为空
 
         console.log(items)
-         
+
     }
 
     const deleteItem = (id) => {
-        const newArray = items.filter(item => item.id!== id)   // filter掉掉id为id的元素
+        const newArray = items.filter(item => item.id !== id)   // filter掉掉id为id的元素
         setItems(newArray)   // 也是传入更新updated items array
+    }
+
+    const moveCursor = (id) => {
+        // Your edit button logic
+        // Set focus to the edit input
+        // 并且更新当前的edititem的id为click的item的id
+        editInputRef.current.focus();
+        console.log(id)
+
+        seteditItemID(id)
+    };
+
+    const editItem = (id) => {
+        const editArray = items.map(item => {
+
+            if (item.id === id) {
+                return { ...item, value: newEditItem }  // 更新item的值
+            }
+
+            else {
+                return item
+            }
+        })
+
+        setItems(editArray)
+        setnewEditItem("")
+        seteditItemID(0)
+
+        console.log(items)
     }
 
 
     return (
-        <div>
-            <input 
-                type="text" 
-                value={newItem}  
-                placeholder="Add a task" 
-                onChange = {e=>setNewItem(e.target.value)} // 没有onchange你是输入不了的，传入当前输入的value
+        <Fragment>
+            <input
+                type="text"
+                value={newItem}   // 当前输入值
+                placeholder="Add a task"
+                onChange={e => setNewItem(e.target.value)} // 没有onchange你是输入不了的，传入当前输入的value
             />
 
-            <button onClick={addItem}> Add a task </button>  
+            <button onClick={addItem}> Add a task </button>
 
+            <div className='mt-2' />
+            <input
+                type="text"
+                value={newEditItem}
+                placeholder='Edit a task'
+                onChange={e => setnewEditItem(e.target.value)} 
+                ref={editInputRef}
+            />
+
+            <button onClick={()=>editItem(editItemID)}> Edit a task </button>
+
+            <div className='mt-3' />
             <ul>
-                 {items.map((item) =>{
+                {items.map((item) => {
                     return (
                         <li key={item.id}>
-                            
+
                             {item.value}
 
-                            <button onClick={()=>deleteItem(item.id)}>❌</button>
+                            <button onClick={() => moveCursor(item.id)}>✏</button>  {/* 光标跳转到input box，且传入id */}
+                            <button onClick={() => deleteItem(item.id)}>❌</button>
 
-                        </li> 
+                        </li>
                     )
-                    
-                 })}
+
+                })}
             </ul>
 
 
-        </div>
+        </Fragment>
 
     )
 
